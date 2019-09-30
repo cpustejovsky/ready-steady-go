8e780f3964169cdb6fbc0ef0cd3eac87b80047d9
cpustejovsky <charles.pustejovsky@gmail.com>
2019-09-30 07:33:15 -0400

    learning about receiver functions and creating custom types or whatever the term is

--- cards/deck.go
+++ cards/deck.go
@@ -1,3 +1,17 @@
+//-----------------------------------------------------------------------------
 package main
 
+import "fmt"
 
+// Create a new type of "deck" which is a slice of strings
+
+type deck []string
+
+//receiver function: any variable of type ___ gets access to the method defined
+//by convention, the variable has the first letter of the type, so "d" in case 
+//of type "deck"
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
