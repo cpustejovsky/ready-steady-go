e30c9ba0d5cf32cdbcc138d888168a2b71806bc1
cpustejovsky <charles.pustejovsky@gmail.com>
2019-10-11 06:47:29 -0400

    working with net/http and interfaces and readers

---
+++ http-req/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	data := httpFetcher("https://www.cpustejovsky.com")
+	fmt.Printf("%s", data)
+}
+
+func httpFetcher(url string) []byte {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Got a nil\n here's the error:\n%v", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	return body
+}
