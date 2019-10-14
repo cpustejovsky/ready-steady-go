4fefcd06f3831873d62d2ad4d927e2cc8c91dcd3
cpustejovsky <charles.pustejovsky@gmail.com>
2019-10-14 05:30:35 -0400

    using for loop to make sure enough channels to match items in links slice

--- channels/main.go
+++ channels/main.go
@@ -14,13 +14,17 @@ func main() {
 		"https://amazon.com",
 		"https://bitpay.com/resources",
 	}
-
+	// Too many channels will make your program hang up
 	c := make(chan string)
 
 	for _, link := range links {
 		go checkLink(link, c)
 	}
-	fmt.Println(<-c)
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
+
 	fmt.Println("all done!")
 }
 
@@ -31,10 +35,10 @@ func checkLink(link string, c chan string) {
 		c <- "Error Occurred"
 	}
 	if resp.StatusCode == 200 {
-		fmt.Printf("%v is working.\n Status Code %v\n", link, resp.StatusCode)
+		fmt.Printf("%v is working.\nStatus Code %v\n", link, resp.StatusCode)
 		c <- "should be working"
 	} else {
-		fmt.Printf("%v is not working.\n Status Code: %v\n", link, resp.StatusCode)
+		fmt.Printf("%v is not working.\nStatus Code: %v\n", link, resp.StatusCode)
 		c <- "should NOT be working"
 	}
 }
