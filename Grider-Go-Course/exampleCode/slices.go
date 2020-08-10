2ae9cb550e1ba62af22d998da3198bd97382e639
cpustejovsky <charles.pustejovsky@gmail.com>
2020-08-10 18:53:32 -0400

    switching to switch statement

--- Grider-Go-Course/exampleCode/slices.go
+++ Grider-Go-Course/exampleCode/slices.go
@@ -5,13 +5,12 @@ import "fmt"
 func main() {
 	nums := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	for _, num := range nums {
-		if num == 0 {
+		switch {
+		case num == 0:
 			fmt.Printf("%v is zero\n", num)
-		}
-		if num%2 == 0 && num != 0 {
+		case num%2 == 0 && num != 0:
 			fmt.Printf("%v is even\n", num)
-		}
-		if num%2 == 1 {
+		case num%2 == 1:
 			fmt.Printf("%v is odd\n", num)
 		}
 	}
