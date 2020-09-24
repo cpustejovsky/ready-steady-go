77293bc7825623afbbfba1925e6d29b653816cfd
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-24 10:01:13 -0400

    reformatting folders; adding a go mod

---
+++ GoPrgrammingLanguage/ch2/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cpustejovsky/ready-steady-go/tree/master/GoPrgrammingLanguage/ch2/tempconv"
+)
+
+func main() {
+	fmt.Printf("Brrrr!	%v\n", tempconv.AbsoluteZeroC)
+}
---
+++ GoPrgrammingLanguage/go.mod
@@ -0,0 +1,3 @@
+module github.com/cpustejovsky/ready-steady-go/tree/master/GoPrgrammingLanguage
+
+go 1.13
