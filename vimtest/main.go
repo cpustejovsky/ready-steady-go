7f998dfc04c66dab31c5023f948a613573f202bc
Charles Clinton Pustejovsky III <charles.pustejovsky@gmail.com>
2023-04-25 13:08:35 -0400

    testing vim

---
+++ vimtest/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("vim-go")
+	fmt.Println("what hath God wrought?")
+	log.Println("what hath God wrought?")
+	//TODO: figure out how to autocomplkete methods and iferr
+}
