d6135a4a6015bee5b090d2e4cfa06122eec9e141
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2023-11-24 21:37:52 -0500

    feat: use channels to make sleep sort a workable algorithm

--- clrs/sleep.go
+++ clrs/sleep.go
@@ -1,20 +1,26 @@
 package clrs
 
 import (
-	"fmt"
 	"sync"
 	"time"
 )
 
-func SleepSort(arr []int) {
+func SleepSort(arr []int) []int {
 	var wg sync.WaitGroup
+	var nums = make(chan int, len(arr))
+	var sorted []int
 	for _, num := range arr {
 		wg.Add(1)
 		go func(n int) {
 			defer wg.Done()
-			time.Sleep(time.Duration(n) * time.Second)
-			fmt.Println(n)
+			time.Sleep(time.Duration(n) * time.Millisecond)
+			nums <- n
 		}(num)
 	}
 	wg.Wait()
+	close(nums)
+	for num := range nums {
+		sorted = append(sorted, num)
+	}
+	return sorted
 }
--- clrs/sleep_test.go
+++ clrs/sleep_test.go
@@ -6,6 +6,16 @@ import (
 )
 
 func TestSleepSort(t *testing.T) {
-	input := []int{5, 2, 4, 6, 1, 3, 10}
-	clrs.SleepSort(input)
+	input := []int{5, 2, 4, 6, 1, 3}
+	want := []int{1, 2, 3, 4, 5, 6}
+	got := clrs.SleepSort(input)
+	if !EqualIntSlices(got, want) {
+		t.Fatalf("got:\t%v\nwant:\t%v\n", got, want)
+	}
+	input = []int{31, 41, 59, 26, 41, 58}
+	want = []int{26, 31, 41, 41, 58, 59}
+	got = clrs.SelectionSort(input)
+	if !EqualIntSlices(got, want) {
+		t.Fatalf("got:\t%v\nwant:\t%v\n", got, want)
+	}
 }
